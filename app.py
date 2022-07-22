6c333a2ba65aca591c829253d9498bfcaf6b5de5
Kaustav Goswami <75466816+Kaustav-Goswami9@users.noreply.github.com>
2022-07-22 12:53:21 +0530

    Created app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.write('''
+# Max of the 3 numbers
+
+This app finds the max of the 3 number entered by the user''')
+
+x1 = int(st.number_input("First Number"))
+x2 = int(st.number_input("Second Number"))
+x3 = int(st.number_input("Third Number"))
+
+st.write('Max number =', max(x1,x2,x3))
